7015d025c0c71cf9c6fb4eeb2b84c6a678c06057
ChanceZhaoquanfeng <18348370447@163com>
2018-03-19 00:22:28 +0800

    springboot

---
+++ comic-animation/src/main/java/com/zqf/manga/ComicAnimationApplication.java
@@ -0,0 +1,20 @@
+package com.zqf.manga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/manga")
+public class ComicAnimationApplication {
+
+	@RequestMapping("/hello")
+	String sayHello(){
+		return "Hello,World!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ComicAnimationApplication.class, args);
+	}
+}
---
+++ comic-animation/src/test/java/com/zqf/manga/ComicAnimationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zqf.manga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComicAnimationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
