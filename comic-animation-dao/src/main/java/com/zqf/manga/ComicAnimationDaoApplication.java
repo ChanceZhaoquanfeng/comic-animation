ccb5961aa2a62e601e150b9faf946cd5c02bdd93
ChanceZhaoquanfeng <18348370447@163com>
2018-03-18 23:12:42 +0800

    项目框架初建

---
+++ comic-animation-dao/src/main/java/com/zqf/manga/ComicAnimationDaoApplication.java
@@ -0,0 +1,12 @@
+package com.zqf.manga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicAnimationDaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicAnimationDaoApplication.class, args);
+	}
+}
---
+++ comic-animation-dao/src/test/java/com/zqf/manga/ComicAnimationDaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zqf.manga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComicAnimationDaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
